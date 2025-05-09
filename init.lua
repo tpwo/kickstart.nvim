5fc4caa181af2c29e9b899351d95c0001387b76f
Tomasz Wojdat <tpwojdat@gmail.com>
2025-05-09 13:29:47 +0200

    Configure pyright to be installed (but npm has to be present)
    Also add python and toml in treesitter configs.

--- init.lua
+++ init.lua
@@ -674,7 +674,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -947,7 +947,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'toml' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
