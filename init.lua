2345c2276ca79e9838d7378cb3f364743efea386
Tomasz Wojdat <tpwojdat@gmail.com>
2025-05-09 13:52:13 +0200

    Turn on isort and ruff formatting on save of Python files
    I'm pushing it to branch now, as ruff works also as a linter with
    autofixes, which is annoying, as e.g. it removes unused imports.

--- init.lua
+++ init.lua
@@ -762,7 +762,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = 'fallback',
           }
         end
@@ -770,7 +770,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
