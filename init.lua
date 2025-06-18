68415132478b592495dfec236cf2f3cdb9ebe145
Tomasz Wojdat <tpwojdat@gmail.com>
2025-06-18 12:26:18 +0200

    Define `dd` binding to delete an item from the quickfix/loc list
    Solution is taken from StackOverflow.
    
    Source: https://stackoverflow.com/a/77181885

--- init.lua
+++ init.lua
@@ -192,6 +192,34 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- quickfix list delete keymap
+function Remove_qf_item()
+  local curqfidx = vim.fn.line '.'
+  local qfall = vim.fn.getqflist()
+
+  -- Return if there are no items to remove
+  if #qfall == 0 then
+    return
+  end
+
+  -- Remove the item from the quickfix list
+  table.remove(qfall, curqfidx)
+  vim.fn.setqflist(qfall, 'r')
+
+  -- Reopen quickfix window to refresh the list
+  vim.cmd 'copen'
+
+  -- If not at the end of the list, stay at the same index, otherwise, go one up.
+  local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
+
+  -- Set the cursor position directly in the quickfix window
+  local winid = vim.fn.win_getid() -- Get the window ID of the quickfix window
+  vim.api.nvim_win_set_cursor(winid, { new_idx, 0 })
+end
+
+vim.cmd 'command! RemoveQFItem lua Remove_qf_item()'
+vim.api.nvim_command 'autocmd FileType qf nnoremap <buffer> dd :RemoveQFItem<cr>'
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
