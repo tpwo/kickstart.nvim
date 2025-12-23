ee763fee36f000e9fb19f522e894eca6fd9c12de
Tomasz Wojdat <tpwojdat@gmail.com>
2025-12-23 16:49:58 +0100

    Auto-format Python files on save

--- init.lua
+++ init.lua
@@ -907,8 +907,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        -- `ruff_format` taken from the docs:
+        -- https://docs.astral.sh/ruff/editors/setup/#neovim
+        python = { 'isort', 'ruff_format' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
