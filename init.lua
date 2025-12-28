f121d066698ee1e7d216ef61379151b3f46cf610
Tomasz Wojdat <tpwojdat@gmail.com>
2025-12-28 22:30:19 +0100

    Enable golang LSP and auto-formatting

--- init.lua
+++ init.lua
@@ -859,7 +859,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -958,6 +958,7 @@ require('lazy').setup({
         -- `ruff_format` taken from the docs:
         -- https://docs.astral.sh/ruff/editors/setup/#neovim
         python = { 'isort', 'ruff_format' },
+        golang = { 'gofmt' },
         javascript = { 'biome' },
         typescript = { 'biome' },
         --
