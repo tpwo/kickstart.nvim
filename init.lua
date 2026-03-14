c191ebf8c98ea45e9f9a0986716ed7940157fb87
Tomasz Wojdat <tpwojdat@gmail.com>
2026-03-14 21:45:00 +0100

    feat: disable formatting on save (use <leader>f instead)
    It works poorly with my work machine, and quick to reach shortcut is
    more than enough.

--- init.lua
+++ init.lua
@@ -1004,20 +1004,6 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 1000,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- `ruff_format` taken from the docs:
